92cbebe1991afaef5f1e530c9078232a47e602c6
Veljko Kokanovic <vkokanovic@mywebgrocer.com>
2019-08-18 00:40:56 +0100

    added comments and nuget package info

--- src/domainD/IAggregateRoot.cs
+++ src/domainD/IAggregateRoot.cs
@@ -6,14 +6,32 @@ namespace domainD
 {
     public interface IAggregateRoot : IEventDispatcher
     {
+        /// <summary>
+        /// Replays all uncommited events and applies action supplied
+        /// </summary>
+        /// <param name="action"></param>
         void Subscribe(Action<DomainEvent> action);
 
+        /// <summary>
+        /// Replays all uncommited events and applies async action supplied
+        /// </summary>
+        /// <param name="action"></param>
         void Subscribe(Func<DomainEvent,Task> action, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Dispatches event to corrent event handler
+        /// </summary>
+        /// <param name="event"></param>
         void Handle(DomainEvent @event);
 
+        /// <summary>
+        /// Clears all uncommited events
+        /// </summary>
         void ClearEvents();
 
+        /// <summary>
+        /// Aggregate root version
+        /// </summary>
         long Version { get; }
     }
 }
